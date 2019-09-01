b516f751dfee33ee22f7ec522e88a56e4d493e3b
Xabier <xabier.parada@gmail.com>
2019-09-01 09:50:58 +0200

    Definida la prueba para la funcion sumar

--- test/unir/CalculadoraTest.java
+++ test/unir/CalculadoraTest.java
@@ -1,10 +1,19 @@
 package unir;
+import org.junit.Assert;
 import org.junit.Test;
 public class CalculadoraTest {
 
 @Test
     public void testSuma(){
-
+    //Arrange
+    int a = 2;
+    int b = 3;
+    int esperado = 5;
+    Calculadora calc = new Calculadora();
+    //Act
+int resultado = calc.sumar(a,b);
+    //Assert
+    Assert.assertEquals(esperado,resultado);
 }
 
 }

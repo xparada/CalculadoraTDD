4e9fc8125afff9e2fcb9f9a2da8f8fd1c010449f
Xabier <xabier.parada@gmail.com>
2019-09-01 10:37:20 +0200

    Implementada la funcion restar

--- src/unir/Calculadora.java
+++ src/unir/Calculadora.java
@@ -4,4 +4,7 @@ public class Calculadora {
     public int sumar(int a, int b){
         return a+b;
     }
+    public int restar(int a, int b){
+        return a-b;
+    }
 }
--- test/unir/CalculadoraTest.java
+++ test/unir/CalculadoraTest.java
@@ -3,18 +3,18 @@ import org.junit.Assert;
 import org.junit.Test;
 public class CalculadoraTest {
 
-@Test
+    @Test
     public void testSuma(){
-    //Arrange
-    int a = 2;
-    int b = 3;
-    int esperado = 5;
-    Calculadora calc = new Calculadora();
-    //Act
-int resultado = calc.sumar(a,b);
-    //Assert
-    Assert.assertEquals(esperado,resultado);
-}
+        //Arrange
+        int a = 2;
+        int b = 3;
+        int esperado = 5;
+        Calculadora calc = new Calculadora();
+        //Act
+        int resultado = calc.sumar(a,b);
+        //Assert
+        Assert.assertEquals(esperado,resultado);
+    }
     @Test
     public void testResta(){
         //Arrange
